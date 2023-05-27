3781c7eb352e219136a87bf9d841df3a2b062c16
unknown <navaneethnavi333@gmail.com>
2023-05-27 20:53:05 +0530

    added functions to generate random quote

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+function getRandomAnimeQuote() {
+  var quoteContainer = document.getElementById("quoteContainer");
+  quoteContainer.innerHTML = "Loading...";
+
   fetch("https://animechan.vercel.app/api/random")
     .then(function(response)
      {
       return response.json();
+    })
+    .then(function(data) {
+      var quoteText = data.quote;
+      var animeTitle = data.anime;
+      var characterName = data.character;
+
+      quoteContainer.innerHTML = "";
+      var quoteElement = document.createElement("p");
+      quoteElement.textContent = `"${quoteText}"`;
+      quoteContainer.appendChild(quoteElement);
+
+      var detailsElement = document.createElement("p");
+      detailsElement.innerHTML = `Anime: ${animeTitle}<br>Character: ${characterName}`;
+      quoteContainer.appendChild(detailsElement);
     })
